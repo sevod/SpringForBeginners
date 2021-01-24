bee296f244947aecd8e95218712bef4d97b12ed9
Sergey Demidov <ssevod@gmail.com>
2021-01-25 00:01:06 +0300

    DI через конструктор

--- spring_introduction/spring_introduction/src/main/java/org/sevod/spring_introducion/Cat.java
+++ spring_introduction/spring_introduction/src/main/java/org/sevod/spring_introducion/Cat.java
@@ -1,6 +1,10 @@
 package org.sevod.spring_introducion;
 
 public class Cat implements Pet{
+    public Cat() {
+        System.out.println("Cat Bean is created");
+    }
+
     @Override
     public void say() {
         System.out.println("Мяу! Мяу!");
