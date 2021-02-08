2221bb5f7186321752980e785d17acb0a3b8fe57
Sergey Demidov <ssevod@gmail.com>
2021-02-08 17:31:34 +0300

    @RequestParam

--- org.sevod.spring.mvc/src/main/java/org/sevod/spring/mvc/MyController.java
+++ org.sevod.spring.mvc/src/main/java/org/sevod/spring/mvc/MyController.java
@@ -3,6 +3,7 @@ package org.sevod.spring.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -22,14 +23,23 @@ public class MyController {
 //    public String showEmployeeDetails(){
 //        return "show-emp-details-view";
 //    }
+//    @RequestMapping("/showDetails")
+//    public String showEmployeeDetails(HttpServletRequest request, Model model){
+//        String empName = request.getParameter("emloyeeName");
+//        empName = "Mr. " + empName;
+//
+//        model.addAttribute("nameAttribute", empName);
+//        model.addAttribute("myAttribute", "It's test!");
+//
+//        return "show-emp-details-view";
+//    }
+
     @RequestMapping("/showDetails")
-    public String showEmployeeDetails(HttpServletRequest request, Model model){
-        String empName = request.getParameter("emloyeeName");
-        empName = "Mr. " + empName;
+    public String showEmployeeDetails(@RequestParam("emloyeeName") String empName, Model model){
+        empName = "Mr. " + empName + "!";
 
         model.addAttribute("nameAttribute", empName);
         model.addAttribute("myAttribute", "It's test!");
-
 
         return "show-emp-details-view";
     }
